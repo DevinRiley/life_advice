91f5d0721e647c6d476b82c3864c1453362f7929
Daniel Pennypacker <danielpennypacker@gmail.com>
2014-02-23 13:11:32 -0800

    making look nicer

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,7 +5,37 @@
 angular.module('app.controllers', []).
   controller('appController', [ '$scope', function($scope) {
     $scope.emails = window.data;
-    console.log($scope.emails);
+
+    // tags is needed in the sidebar anf the main content, so setting up here
+    $scope.createTags = function() {
+        $scope.tags = []; //the tag object we care about and used in UI
+        var tagCount = {}; //helper hash!
+
+        // loop over all the emails
+        angular.forEach($scope.emails, function(value, key) {
+
+            //loop over each tag in an email
+            angular.forEach( value.tags, function(value, key){
+                if(!tagCount[value]) {
+                    tagCount[value] = 0;
+                    // - create the tag object
+                    $scope.tags.push({
+                        name: value,
+                        selected : false
+                    });
+                }
+                tagCount[value] += 1; // up the tags value
+            });
+        });
+
+        // set the final count on all of the tags
+        angular.forEach( $scope.tags , function(tag, i) {
+            tag.count = tagCount[tag.name];
+        });
+    };
+    $scope.createTags(); //invoke immediately
+
+
   }])
   .controller('sidebarController', [ '$scope', function($scope) {
     $scope.openPosition = 0;
@@ -15,23 +45,19 @@ angular.module('app.controllers', []).
 
   }])
   .controller('filterController', [ '$scope', function($scope) {
-
-    $scope.setTags = function() {
-        $scope.tags = {};
-        angular.forEach($scope.emails, function(value, key) {
-        angular.forEach( value.tags, function(value, key){
-          if(!$scope.tags[value]) {
-            $scope.tags[value] = 0; 
-          }
-          $scope.tags[value] += 1;
-        })
-      });
-    };
-
-    $scope.setTags();
+        $scope.toggleTag = function(tag){
+            tag.selected = !tag.selected;
+        }
 
   }])
   .controller('emailShortController', [ '$scope', function($scope) {
+
+   /* ------
+    NOTE: "email" is being set in an "ng-repeat" in the dom
+    TODO I think there's an easy way to write in the controller
+    that we're expecting people to do that. like "require:" or something
+    */
+
     $scope.openEmail = function() {
       if( $scope.email.open ) {
         return false;
@@ -39,14 +65,27 @@ angular.module('app.controllers', []).
       $scope.email.open = true;
       $scope.email.position = $scope.openPosition;
       $scope.incrementPosition();
-      console.log($scope.email.position);
     }
 
+    $scope.closeEmail = function() {
+        $scope.email.open = false;
+    }
+
+    // this ends up being helpful to have
+    $scope.toggleEmail = function(){
+        if($scope.email.open) {
+            $scope.closeEmail()
+        } else {
+            $scope.openEmail()
+        }
+    }
 
   }])
   .controller('contentController', [ '$scope', function($scope) {
   }])
   .controller('emailFullController', [ '$scope', function($scope) {
+     // find way to not repeat this. I don't think angular is as into
+     // inheritance, but need to look up
      $scope.closeEmail = function() {
        $scope.email.open = false;
      }
--- app/js/data.js
+++ app/js/data.js
@@ -2,7 +2,7 @@ window.data = [
   { 
     open: false,
     position: 1,
-    tags: ['recommended'],
+    tags: ['recommended','the matrix'],
     title: "What to do with your life",
     message: "This is a really inspiring message about what to do with yourself."
   },
@@ -10,7 +10,7 @@ window.data = [
   { 
     open: false,
     position: 2,
-    tags: ['recommended'],
+    tags: ['recommended', 'the matrix'],
     title: "Second message",
     message: "Hey, join a relgion breh."
   },
@@ -19,7 +19,31 @@ window.data = [
     open: false,
     position: 3,
     tags: ['recommended', 'religion'],
-    title: "You are making a big mistake",
-    message: "Cool."
-  }
+    title: "So whining",
+    message: "you're lame."
+  },
+
+    {
+        open: false,
+        position: 1,
+        tags: ['the matrix'],
+        title: "See ya later",
+        message: "nothing is permanent"
+    },
+
+    {
+        open: false,
+        position: 2,
+        tags: ['the matrix'],
+        title: "This is Morpheous",
+        message: "Hello Neo."
+    },
+
+    {
+        open: false,
+        position: 3,
+        tags: ['meditation', 'religion'],
+        title: "Meet the Buddah",
+        message: "Kill the Buddah."
+    }
 ]
